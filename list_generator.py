5d1839e21ed65ac1c10dd80a8fec82b10553e349
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2021-06-12 23:45:02 +0600

    Update list_generator.py

--- list_generator.py
+++ list_generator.py
@@ -13,12 +13,12 @@ ipList = []
 taskList = []
 
 def fetch_ip(url):
-	
-	#get ip
-	ip = get_ip( url )
-	
-	# append ip for listing
-	ipList.append( ip )
+  
+  #get ip
+  ip = get_ip( url )
+  
+  # append ip for listing
+  ipList.append( ip )
 
 # download the youtubeparsed list
 download('https://raw.githubusercontent.com/nickspaargaren/no-google/master/categories/youtubeparsed', 'youtubeparsed')
@@ -26,33 +26,33 @@ download('https://raw.githubusercontent.com/nickspaargaren/no-google/master/cate
 
 # open the youtubeparsed file
 with open('youtubeparsed', mode = 'r', encoding = 'utf-8') as f:
-	
-	# for each url in the file
-	for url in f.readlines():
-		
-		# ignore empty lines
-		if url == '':
-			continue
-		
-		# ignore if '#' character is found
-		if url[0] == '#':
-			continue
-		
-		# strip whitespaces and '.'
-		url = url.strip().lstrip('.')
-		
-		# make a thread that will save the ip
-		# ... and save it to taskList
-		taskList.append(Thread(target=fetch_ip, args=(url,)))
-		
+  
+  # for each url in the file
+  for url in f.readlines():
+    
+    # ignore empty lines
+    if url == '':
+      continue
+    
+    # ignore if '#' character is found
+    if url[0] == '#':
+      continue
+    
+    # strip whitespaces and '.'
+    url = url.strip().lstrip('.')
+    
+    # make a thread that will save the ip
+    # ... and save it to taskList
+    taskList.append(Thread(target=fetch_ip, args=(url,)))
+    
 # start the tasks
 for t in taskList:
-		t.start()
+    t.start()
 
 # wait for the tasks to end
 for t in taskList:
-		t.join()
-		
+    t.join()
+    
 
 # make sure no repeating ip is available
 # this line of code will remove repeatation
@@ -68,5 +68,5 @@ print(*ipList, sep='\n')
 # this will create a file 'youtube_iplist.txt'
 # ... with the list of the ips
 with open('iplist.txt', mode = 'w', encoding = 'utf-8') as f:
-	
-	f.write('\n'.join(ipList))
+  
+  f.write('\n'.join(ipList))
