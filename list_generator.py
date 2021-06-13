328647a394cb769706b1365fbad80143251e9fc3
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2021-06-13 13:23:51 +0000

    Create list_generator.py

--- list_generator.py
+++ list_generator.py
@@ -50,7 +50,7 @@ with open('youtubeparsed', mode = 'r', encoding = 'utf-8') as f:
     #taskList.append(Thread(target=fetch_ip, args=(url,)))
     try:
       fetch_ip(url)
-    else:
+    except:
       pass
     
 # start the tasks
