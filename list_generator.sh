1394098b4ee401ad32edc8a6cc52c078aae9df03
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2021-12-22 19:31:45 +0600

    Update list_generator.sh

--- list_generator.sh
+++ list_generator.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/nickspaargaren/no-google/master/categories/youtubeparsed
+wget 'https://raw.githubusercontent.com/nickspaargaren/no-google/master/categories/youtubeparsed'
+echo "$(grep -oP '^([\w\d.-]+\.)+([\w\d.-]+)?' youtubeparsed)" > youtubeparsed
 
-dig +short -4 -f youtubeparsed >> ipv4_list.txt
-dig +short -6 -f youtubeparsed >> ipv6_list.txt
+parallel -a youtubeparsed '\
+line="{}"
+if [[ ! (-z "$line" || $line =~ \#) ]]; then
+  echo "digging $line ..."
+  dig +noall +short $line -4 >> ipv4_list.txt
+  dig +noall +short $line -6 >> ipv6_list.txt
+fi
+'
 
 sort -t . -k 1,1n -k 2,2n -k 3,3n -k 4,4n ipv4_list.txt -o ipv4_list.txt
 sort ipv6_list.txt -o ipv6_list.txt # for now
