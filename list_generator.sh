e18d60c78321a36a7ba61fc18312450e0d5bdeab
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2021-12-22 17:11:58 +0600

    write list_generator.sh

---
+++ list_generator.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+YOUTUBE_PARSED='https://raw.githubusercontent.com/nickspaargaren/no-google/master/categories/youtubeparsed'
+YOUTUBE_HOSTNAMES=$(curl -G $YOUTUBE_PARSED)
+
+echo $YOUTUBE_HOSTNAMES | dig +short -4 >> ipv4_list.txt
+echo $YOUTUBE_HOSTNAMES | dig +short -6 >> ipv6_list.txt
+
+sort -t . -k 1,1n -k 2,2n -k 3,3n -k 4,4n ipv4_list.txt -o ipv4_list.txt
+sort -t : ipv6_list.txt -o ipv6_list.txt # for now
